20f732f434311b1154059c54d740b408efbcc020
yassinfayed <yassin_fayed@hotmail.com>
2023-05-29 02:10:08 +0300

    part 2

--- M3/src/index.js
+++ M3/src/index.js
@@ -9,12 +9,25 @@ import MultiStepForm from './Loan';
 import Credit from './Credit';
 import Open from './Open';
 import CreditTrans from './CreditTrans';
-import CreditCardPage from './CreditCardPage'
 import Replacement from './Replacement';
 import Account from './Account';
 import BillingPage from './BillingPage';
-import NotificationsPage from './Notifications';
-
+import BankerHome from './bankerHome';
+import BankerCreditRequests from './BankerCreditRequests';
+import BankerLoanRequests from './BankerLoanRequests';
+import BankerOpenRequests from './BankerOpenRequests';
+import BankerReplacementRequests from './BankerReplacementRequests';
+import BankerRequests from './BankerRequests';
+import BankerLogin from './BankerLogin';
+import ReportIssues from './ReportIssues';
+import NewDesign from './NewDesign';
+import NewLoan from './newLoans';
+import NewCreditCard from './NewCreditCard';
+import BankTrans from './BankTrans';
+import CarLoan from './CarLoan';
+import BankerIssues from './BankerIssues'
+import BankerSettings from './BankerAccount';
+import Close from './Close';
 
 
 ReactDOM.render(
@@ -27,12 +40,30 @@ ReactDOM.render(
         <Route path="/applyforpersonalloan" element={<MultiStepForm />} />
         <Route path="/applyforcredit" element={<Credit />} />
         <Route path="/open" element={<Open />} />
+        <Route path="/close" element={<Close />} />
         <Route path="/creditcardtransactions" element={<CreditTrans />} />
+        <Route path="/bankaccounttransactions" element={<BankTrans />} />
         <Route path="/applyforreplacement" element={<Replacement />} />
-        <Route path="/mycreditcards" element={<CreditCardPage />} />
+        <Route path="/mycreditcards" element={<NewCreditCard />} />
         <Route path="/acc" element={<Account />} />
-        <Route path="/mybills" element={<BillingPage />} />   
-        <Route path="/notifications" element={<NotificationsPage />} />   
+        <Route path="/mybills" element={<BillingPage />} />
+        <Route path="/myaccounts" element={<NewDesign/>} />
+        <Route path="/myloans" element={<NewLoan/>} />
+        <Route path="/banker" element={<BankerLogin></BankerLogin>} />
+        <Route path="/bankerHome" element={<BankerHome></BankerHome>} />
+        <Route path="/bankerrequests" element={<BankerRequests></BankerRequests>} />
+        <Route path="/bankerLoanrequests" element={<BankerLoanRequests></BankerLoanRequests>} />
+        <Route path="/bankerCCrequests" element={<BankerCreditRequests></BankerCreditRequests>} />
+        <Route path="/bankerOpenrequests" element={<BankerOpenRequests></BankerOpenRequests>} />
+        <Route path="/bankerReplacementrequests" element={<BankerReplacementRequests></BankerReplacementRequests>} />
+        <Route path="/reports" element={<ReportIssues/>} />
+        <Route path="/applyforcar" element={<CarLoan/>} />
+        <Route path="/bankerissues" element={<BankerIssues/>} />
+        <Route path="/bankeraccount" element={<BankerSettings/>} />
+
+
+
+
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
